17db7c0ddcf1ca5b60345330d5c9034f334a4469
Viraj Chitnis <chitnisviraj@gmail.com>
2023-04-30 16:29:55 +0000

    Added ability to parse commandline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // Parse command line arguments
+    let arguments = std::env::args();
+    let arguments = arguments::parse(arguments).unwrap();
+    
+    // Get command line argument values
+    let configfile = arguments.get::<String>("config").unwrap_or("./config.yml".to_string());
+
+    // Start execution logic
+    println!("Backvol starting up...");
+    println!("Config file location: {configfile}");
+    println!("Backvol completed.");
 }
