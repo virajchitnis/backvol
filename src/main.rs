f86a060421228857defc6c49f4a2d4182ce5539b
Viraj Chitnis <chitnisviraj@gmail.com>
2023-04-30 21:05:59 +0000

    Added ability to parse yml config file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+extern crate yaml_rust;
+use std::fs;
+use yaml_rust::{YamlLoader};
+
 fn main() {
     // Parse command line arguments
     let arguments = std::env::args();
     let arguments = arguments::parse(arguments).unwrap();
     
     // Get command line argument values
-    let configfile = arguments.get::<String>("config").unwrap_or("./config.yml".to_string());
+    let configfile = arguments.get::<String>("config").unwrap_or("./example-config.yml".to_string());
 
-    // Start execution logic
     println!("Backvol starting up...");
     println!("Config file location: {configfile}");
+
+    // Load yaml from config file
+    let filecontents = fs::read_to_string(configfile).expect("Should have been able to read the file");
+    let config_yaml = YamlLoader::load_from_str(filecontents.as_str()).unwrap();
+    let config = &config_yaml[0];
+
+    // For debugging
+    println!("{:?}", config);
+
+    let rsync_location = config["rsync_location"].as_str().unwrap_or("rsync");
+    println!("rsync location: {rsync_location}");
+    
     println!("Backvol completed.");
 }
