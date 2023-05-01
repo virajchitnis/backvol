3da549c3aeb05f36c31da978aa820dd8d74f5de4
Viraj Chitnis <chitnisviraj@gmail.com>
2023-05-01 02:49:00 +0000

    Get all necessary properties from config file

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     // Get command line argument values
     let configfile = arguments.get::<String>("config").unwrap_or("./example-config.yml".to_string());
 
-    println!("Backvol starting up...");
+    println!("Backvol starting...");
     println!("Config file location: {configfile}");
 
     // Load yaml from config file
@@ -18,11 +18,32 @@ fn main() {
     let config_yaml = YamlLoader::load_from_str(filecontents.as_str()).unwrap();
     let config = &config_yaml[0];
 
-    // For debugging
-    println!("{:?}", config);
+    println!("rsync: ");
+    let rsync_location = config["rsync"]["location"].as_str().unwrap_or("rsync");
+    println!("\tlocation: {rsync_location}");
 
-    let rsync_location = config["rsync_location"].as_str().unwrap_or("rsync");
-    println!("rsync location: {rsync_location}");
+    let rsync_args = config["rsync"]["args"].as_str().unwrap_or("-a");
+    println!("\targs: {rsync_args}");
+
+    println!("snapper: ");
+    let snapper_enabled = config["snapper"]["enabled"].as_bool().unwrap_or(false);
+    println!("\tenabled: {snapper_enabled}");
+
+    if snapper_enabled {
+        let snapper_location = config["snapper"]["location"].as_str().unwrap_or("snapper");
+        println!("\tlocation: {snapper_location}");
+
+        let snapper_cleanup = config["snapper"]["cleanup"].as_str().unwrap_or("number");
+        println!("\tcleanup: {snapper_cleanup}");
+    }
+
+    println!("Volumes: ");
+    for volume in config["volumes"].clone().into_iter() {
+        let name = volume["name"].as_str().unwrap();
+        let src = volume["src"].as_str().unwrap();
+        let dst = volume["dst"].as_str().unwrap();
+        println!("\t{name} [{src} ===> {dst}]");
+    }
     
     println!("Backvol completed.");
 }
